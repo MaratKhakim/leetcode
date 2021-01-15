7b44fd4efb9ad9b96a3a7be5efee37f51aa2ef79
MaratKhakim <marakhakim@gmail.com>
2021-01-15 20:58:13 +0100

    Get Maximum in Generated Array

---
+++ src/array/GetMaximumInGeneratedArray.java
@@ -0,0 +1,39 @@
+package array;
+
+/**
+ * Date 15.01.2021.
+ *
+ * 1646. Get Maximum in Generated Array
+ *
+ * You are given an integer n. An array nums of length n + 1 is generated in the following way:
+ *       - nums[0] = 0
+ *       - nums[1] = 1
+ *       - nums[2 * i] = nums[i] when 2 <= 2 * i <= n
+ *       - nums[2 * i + 1] = nums[i] + nums[i + 1] when 2 <= 2 * i + 1 <= n
+ * Return the maximum integer in the array nums​​​.
+ *
+ * https://leetcode.com/problems/get-maximum-in-generated-array/
+ */
+
+public class GetMaximumInGeneratedArray {
+    public int getMaximumGenerated(int n) {
+        if (n < 2)
+            return n;
+
+        int[] arr = new int[n+1];
+        arr[1] = 1;
+        int max = 0;
+
+        for (int i = 2; i <= n; i++) {
+            if (i%2 == 0) {
+                arr[i] = arr[i/2];
+            } else {
+                arr[i] = arr[i/2] + arr[i/2 + 1];
+            }
+
+            max = Math.max(max, arr[i]);
+        }
+
+        return max;
+    }
+}
